546ed87cf9349a95595abc3ec7eee3aa21837ab8
prosper <anosikeprosper2@gmail.com>
2023-05-13 14:35:14 -0700

    new: helper fuctions for testing

---
+++ tests/test.helper.js
@@ -0,0 +1,12 @@
+const note = {
+  title: "Fall of the Ocean",
+  body: "Hearld grift thoughts",
+};
+const user = {
+  email: "userone@gmail.com",
+  password: "password",
+};
+
+const invalidId = "63f5022ff0b2c6f5c4cf5fb9";
+
+module.exports = { note, user, invalidId };
